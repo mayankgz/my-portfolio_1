3cbad88ea092df7b69a14c188681a7d7e11c9653
Mayank Gagneja <mayankgagneja2002@gmail.com>
2023-07-28 19:24:29 +0530

    final_final

--- src/Components/Modules/Projects.jsx
+++ src/Components/Modules/Projects.jsx
@@ -13,11 +13,11 @@ function Projects() {
             <h1 className='heading_123'>My WoRKS</h1>
             <div className='project_page'>
                 <Card2 src={ai} txt="Real Time Chat Application using Mern Stack" anc="https://github.com/mayankgz/We---Connect-final" button_val="GitHub Repo"/>
-                <Card2 src={gspot} txt="An E-commerce Website for buying programming courses with search engine and API integration" anc="www.google.com" button_val="GitHub Repo"/>
-                <Card2 src={campus} txt="Campus Block,BVCOE official website. which is built from scratch" anc="www.google.com" button_val="Live Link"/>
-                <Card2 src={api} txt="An experimental web page with search engine using giphy API" anc="www.google.com" button_val="GitHub Repo"/>
-                <Card2 src={jwt} txt="An Advance Authentication enabled website using passport JWT(Java Web Token)" anc="www.google.com" button_val="GitHub Repo"/>
-                <Card2 src={flight} txt="An Online Flight Booking Web app (under development)" anc="www.google.com" button_val="GitHub Repo"/>
+                <Card2 src={gspot} txt="An E-commerce Website for buying programming courses with search engine and API integration" anc="https://github.com/mayankgz/goalspot-search-engine" button_val="GitHub Repo"/>
+                <Card2 src={campus} txt="Campus Block,BVCOE official website. which is built from scratch" anc="https://campusblocksbvcoe.netlify.app/" button_val="Live Link"/>
+                <Card2 src={api} txt="An experimental web page with search engine using giphy API" anc="https://github.com/mayankgz/Giphy-API" button_val="GitHub Repo"/>
+                <Card2 src={jwt} txt="An Advance Authentication enabled website using passport JWT(Java Web Token)" anc="https://github.com/mayankgz/Authentication-JWT" button_val="GitHub Repo"/>
+                <Card2 src={flight} txt="An Online Flight Booking Web app (under development)" anc="https://github.com/mayankgz/flight_booking" button_val="GitHub Repo"/>
 
             </div>
         </div>
